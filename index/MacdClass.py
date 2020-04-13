849635ac262585d58484beeffe17ab3253058343
dorayunyi <luxi@bonc.com.cn>
2020-04-13 21:25:07 +0800

    修改基类为Index

---
+++ index/MacdClass.py
@@ -0,0 +1,36 @@
+import talib as ta;
+import index.index as ind
+class MACD(ind.Index):
+    #__close = None
+    __diff = None
+    __dea = None
+    __up2u = None
+    __fastperiod = 12
+    __slowperiod = 26
+    __signalperiod = 9
+
+    def __init__(self,sourceData):
+        ind.Index.__init__(sourceData)
+
+    def calculate(self):
+        self.__diff,self.__dea,self.__up2u = ta.MACD(self._sourceData,
+                                                     fastperiod=self.__fastperiod,
+                                                     slowperiod=self.__slowperiod,
+                                                     signalperiod=self.__signalperiod)
+
+    def getDiff(self):
+        return self.__diff
+
+    def getDea(self):
+        return self.__dea;
+
+    def getUp2u(self):
+        return self.__up2u;
+    def setFastPeriod(self,fastperiod):
+        self.__fastperiod = fastperiod
+
+    def setSlowPeriod(self,slowperiod):
+        self.__slowperiod = slowperiod
+
+    def setSignalPeriod(self,signalperiod):
+        self.__signalperiod = signalperiod
